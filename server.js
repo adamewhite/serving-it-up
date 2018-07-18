1e654e8e76e1cf4103a98ddeea34b0b2296b7c4a
adamewhite <evilofbanality@gmail.com>
2018-07-18 11:58:13 -0400

    updated server

--- server.js
+++ server.js
@@ -2,48 +2,29 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var path = require('path');
-var cors = require('cors');
 var app = express();
 var router = express.Router();
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+require('dotenv').config();
+
 //set our port to either a predetermined port number if you have set it up, or 3001
-app.set("port", process.env.PORT || 8080);
+// app.set("port", process.env.PORT || 3001);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(cors());
-
 //db config
-var mongoDB = 'mongodb://aewhite:asby1!@ds233531.mlab.com:33531/heroku_qcqkg4p2';
-mongoose.connect(mongoDB);
+var mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 //body parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-  //and remove cacheing so we get the most recent comments
-  res.setHeader('Cache-Control', 'no-cache');
-  next();
-});
-
-// allow cross-browser
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
-// handling static assets
-// app.use(express.static(path.join(__dirname, 'build')));
 
 // api handling
 var TrustsSchema = new Schema({
@@ -52,7 +33,6 @@ var TrustsSchema = new Schema({
 });
 
 var Trust = mongoose.model('Trust', TrustsSchema);
-
 
 app.get("/api/trusts", (req, res) => {
       Trust.find(function(err, trusts) {
@@ -63,6 +43,11 @@ app.get("/api/trusts", (req, res) => {
       });
 });
 
-app.listen(app.get("port"), () => {
-  console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
+var server = app.listen(process.env.PORT || 3001, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
 });
+
+// app.listen(process.env.PORT || 3001, () => {
+//   console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
+// });
